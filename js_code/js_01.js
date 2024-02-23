b3c6e207c5a6087da9160e6ee4718405ba9d7a42
Heromanik <161047539+Heromanik@users.noreply.github.com>
2024-02-23 15:24:16 +0000

    container config done

--- js_code/js_01.js
+++ js_code/js_01.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+const accountId=258666 
+// const can't change accountId="1245" give an error.
+
+let accountEmail="manik@gmail.com"
+var accountPassword="45555"
+//let and var can modify
+accountCity="kolkata"
+
+accountEmail="mallick@gmail.com"
+accountPassword="123456"
+
+console.log(accountId)
+console.table([accountId,accountEmail, accountPassword,accountCity]);
