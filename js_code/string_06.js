0df8c75b45d9f4c958dfece47394b791edeed85a
Heromanik <161047539+Heromanik@users.noreply.github.com>
2024-02-24 13:28:04 +0000

    string_06

--- js_code/string_06.js
+++ js_code/string_06.js
@@ -3,7 +3,7 @@ const age=25
 console.log("my name is "+name + " and my age is "+ age );//method 1
 console.log(`my name is ${name} and my age is ${age}`);// method 2 
 //that is called string interpolation using '`' backthick and dollar sign
-const gameChange=new String ('manikkkk') // another way to decalre a string but here we can access all keys
+const gameChange=new String ('manik-kkk-mm') // another way to decalre a string but here we can access all keys
 console.log(gameChange[0]); // its return 'm' thats the first char of 'manikkk'
 console.log(gameChange.length);//its return 8 
 console.log(gameChange.toUpperCase());
@@ -21,8 +21,10 @@ const newString3 ="   manik   "
 console.log(newString3);
 console.log(newString3.trim());
 
-
-
+const url="https://manik.com/manik%20mallick"
+console.log(url.replace('%20','-'));
+console.log(url.includes('manik'));// return true
+console.log(gameChange.split('-'));// return [ 'manik', 'kkk', 'mm' ]
 
 
 
