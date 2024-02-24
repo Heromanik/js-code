ad582d087ee96d1ead88869116ec9ce0a1e9a37a
Heromanik <161047539+Heromanik@users.noreply.github.com>
2024-02-24 04:42:17 +0000

    js_03

---
+++ js_code/js_03.js
@@ -0,0 +1,28 @@
+/*let score="33abc"
+console.log(typeof score)
+
+let score1="33"
+console.log(typeof score1)
+ 
+let valueInNumber=Number(score) //its always gives number if value is not a number
+console.log(typeof valueInNumber)
+let valueInNumber1=String(score)
+console.log(typeof valueInNumber1)
+*/
+/*
+if we pass boolean value as a true/false its return 1/0
+if we pass undefined as a value its return NAN
+if we pass null as a value then its return 0
+
+*/
+
+let isLoggedIn="" //return false 
+let isLoggedIn1=0; // return false 
+let isLoggedIn2="manik" //return true
+let isLoggedIn3=1 // return true
+let booleanLoggedIn=Boolean (isLoggedIn2)
+//console.log(booleanLoggedIn) 
+console.log(booleanLoggedIn)  
+//1=true ,0=false
+//"manik"=true, ""=false
+ 
