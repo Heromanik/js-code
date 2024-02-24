c1cec88d0264a8f4761ea9cc0bb65b99014ae1d4
Heromanik <161047539+Heromanik@users.noreply.github.com>
2024-02-24 03:54:17 +0000

    js_02

---
+++ js_code/js_02.js
@@ -0,0 +1,22 @@
+"use treat";//treat all js code as newer version
+
+//alert(3+3) // we are using node js, not browser
+
+let name="manik";
+let age=25
+let isLoggedIn=false 
+/* 
+primitive datatypes
+number =>2 to power 53
+bigint
+string=""
+boolean =true/false
+null=>standalone value
+usdefined=>
+symbol=> unique
+
+non primitive datatype
+object
+*/
+console.log(typeof null) // null as a object
+console.log(typeof undefined) // undefined as a undefined 
