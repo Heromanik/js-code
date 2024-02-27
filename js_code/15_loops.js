0cae158b05a43da3b9d9b7a7676e1f7d96508d6d
Heromanik <161047539+Heromanik@users.noreply.github.com>
2024-02-27 11:09:32 +0000

    loops

---
+++ js_code/15_loops.js
@@ -0,0 +1,38 @@
+//for loop
+
+// for (let index = 0; index < 10; index++) {
+//     console.log(index);
+    
+// }
+
+// for (let i = 1; i <=10; i++) {
+//     console.log(`Table of ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`${i} * ${j} = ${i*j}`);      
+//     }
+    
+// }
+
+// let myarray=["manik","soureen","moya"]
+// for (let i = 0; i < myarray.length; i++) {
+//     const element = myarray[i];
+//     console.log(element);
+    
+// }
+//break , continue
+
+//while,dowhile
+
+// let i=1
+// while (i<10) {
+//     console.log(i);
+//     i+=2;
+// }
+
+let score=11
+
+do {
+console.log(score);
+score++;
+
+} while (score<=10);
