597ed57ded56637993f1af2a21d77e304c580721
Heromanik <161047539+Heromanik@users.noreply.github.com>
2024-02-27 14:15:26 +0000

    loops

--- js_code/15_loops.js
+++ js_code/15_loops.js
@@ -29,10 +29,142 @@
 //     i+=2;
 // }
 
-let score=11
+// let score=11
 
-do {
-console.log(score);
-score++;
+// do {
+// console.log(score);
+// score++;
 
-} while (score<=10);
+// } while (score<=10);
+
+
+
+// for of loop
+
+//["","",""]
+//[{},{},{}]
+
+// const arr=[1,2,3,4,5]
+
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+// const greetings="hello world"
+// for (const greet of greetings) {
+//     console.log(`each char is ${greet}`);
+// }
+
+
+//Maps uses for unique value 
+
+const map=new Map()
+map.set('IN',"India")
+map.set('FR',"France")
+map.set('SI',"Srilanka")
+map.set('IN',"India")
+map.set('FR',"France")
+
+//Map(3) { 'IN' => 'India', 'FR' => 'France', 'SI' => 'Srilanka' }
+
+//console.log(map);
+
+for (const [key, value] of map) {
+    //console.log(key,':-',value);
+/*  IN :- India
+    FR :- France
+    SI :- Srilanka */
+}
+
+const myObject={
+    'game1':'cricket',
+    'game2':'football'
+}
+
+// for (const [key,value] of myObject) {
+//     console.log(key,':-',value); // throws an error 'myObject is not iterable'
+// }
+
+// for in loop
+const myobj={
+    js:"javascript",
+    cpp:"c++",
+    ML:"pythond"
+}
+
+for (const key in myobj) {
+    //console.log(`${key} :-  ${myobj[key]}`);
+}
+
+const programming=["js","rb","py"]
+
+for (const key in programming) {
+    //console.log(programming[key]);
+}
+
+/*js
+  rb
+  py*/
+
+  // for each loop
+  
+
+  const coding=["js","ruby","python","java","c++"]
+
+//   coding.forEach(function (item) {
+//     console.log(item);
+//   })
+    /** js
+ruby
+python
+java
+c++ */
+
+
+coding.forEach((item) =>{
+   // console.log(item); // same
+
+})
+
+function printme(item) {
+    //console.log(item);
+    
+}
+coding.forEach(printme)
+
+coding.forEach((item,index,arr) =>{
+   // console.log(item,` `,index,`  `,arr); 
+ /**
+  
+ js 0 [ 'js', 'ruby', 'python', 'java', 'c++' ]
+ruby 1 [ 'js', 'ruby', 'python', 'java', 'c++' ]
+python 2 [ 'js', 'ruby', 'python', 'java', 'c++' ]
+java 3 [ 'js', 'ruby', 'python', 'java', 'c++' ]
+c++ 4 [ 'js', 'ruby', 'python', 'java', 'c++' ]
+  */
+ })
+
+
+
+ const mycoding=[
+
+    {
+        languagename:"js"
+    },
+    {
+        languagename:"java"
+    },
+    {
+        languagename:"c++"
+    },
+ ]
+
+ mycoding.forEach((item)=>{
+    console.log(item.languagename);
+ })
+
+ /**
+  js
+java
+c++
+  */
