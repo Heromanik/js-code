fecf55b23b12ef7b7b44948a2d9d49f248443f02
Heromanik <161047539+Heromanik@users.noreply.github.com>
2024-02-27 05:36:56 +0000

    function

--- js_code/12_scope.js
+++ js_code/12_scope.js
@@ -10,11 +10,12 @@ const c=30 //// this const variable cannot access outside the if scope
 //console.log(c); //error
 //console.log(b); //not an error 30
 
+//closure -> nested function child function is access the parent object but not vice versa
 function one() {
     const username="manik"
     function two() {
         const website="youtube"
-        console.log(username);
+       // console.log(username);
         
     }
    // console.log(website); // can't access website outside the function scope
@@ -23,3 +24,36 @@ function one() {
     
 }
 one()
+
+if (true) { // parent if
+
+    const username="manik"
+        if(username==="manik") // child if , it can access 'username' under parent if 
+        {
+            const website=" youtube"
+            console.log(username + website);
+        }
+    //console.log(website);
+}
+
+
+//*************interesting  ******************/
+
+// one type to declare a function
+function addone (num) {
+    return num+1
+}
+addone(5) // it also used before function declare
+
+// 2nd type to declare a function using a variable
+
+
+//addTwo(5) // give an error
+const addTwo=function (num) {
+    return num+2
+}
+addTwo(5) // it cant declare before function declare
+
+
+// *********** arrow functions & this keyword *************
+
